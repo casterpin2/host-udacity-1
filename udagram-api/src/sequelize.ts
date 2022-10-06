579742ccdeb751458f4bee3eec9a746ce70cd598
tuyennn3 <casterpin2@gmail.com>
2022-10-06 23:34:23 +0700

    test

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
+  username: 'postgres',
+  password: '12345678',
+  database: 'postgres',
+  host: 'database-1.ce3rctw467yl.us-east-1.rds.amazonaws.com',
 
   dialect: "postgres",
   storage: ":memory:",
